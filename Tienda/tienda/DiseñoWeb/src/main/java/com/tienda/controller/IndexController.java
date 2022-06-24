f4c51f87e7f4b3a5315a89864e3a64dd5256e3f0
Josday74 <98436057+Josday74@users.noreply.github.com>
2022-06-24 07:58:08 -0600

    Tarea completa de Semana 3

---
+++ Tienda/tienda/DiseñoWeb/src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,53 @@
+package com.tienda.controller;
+
+import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author Josday
+ */
+@Controller
+@Slf4j
+public class IndexController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var clientes = clienteService.getClientes();
+        model.addAttribute("clientes", clientes);
+        return "index";
+    }
+
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
+}
